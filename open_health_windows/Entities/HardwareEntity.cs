30695b0dee0e44ebd483ddd88dbbff281b9e1d1b
Dombi Balázs Sándor <dombi.balazs.sandor@gmail.com>
2026-03-06 15:06:21 +0100

    Add initial entities, services, and viewmodels
    Add scaffolded domain entities, service interfaces/implementations, and view models for the Open Health Windows app. Included: ImageEntity (ImagePath, ImageByteArray), ChosenHardwareEntity (HardwareChoice enum with CPU/NPU), empty EvaluationEntity, DataViewModel, HardwareViewModel, and placeholder service files (IEvaluationService, IHardwareService, IImageService/Interface1, EvaluationService, HardwareService, ImageService). These are basic codes to support further implementation and wiring; some interfaces/classes are placeholders to be implemented later.

---
+++ open_health_windows/Entities/HardwareEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open_health_windows.Entities
+{
+    class ChosenHardwareEntity
+    {
+        public enum HardwareChoice
+        {
+            CPU,
+            NPU
+        }
+        public HardwareChoice ChosenHardware { get; set; } = HardwareChoice.CPU;
+    }
+}
---
+++ open_health_windows/Entities/ImageEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open_health_windows.Entities
+{
+    class ImageEntity
+    {
+        public string? ImagePath { get; set; }
+
+        public byte[] ImageByteArray { get; set; } = Array.Empty<byte>();
+
+    }
+}
